2c61ecd93a9f4c7e118b6d4ad4fca299ecb7e8e2
Jordan Hudgens <jed.flynn.09@gmail.com>
2018-01-12 17:10:06 +0000

    Added pages controller and corresponding views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def greenbelts
+  end
+
+  def forms
+  end
+
+  def gold_course
+  end
+
+  def cemetery
+  end
+
+  def community_links
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/greenbelts'
+
+  get 'pages/forms'
+
+  get 'pages/gold_course'
+
+  get 'pages/cemetery'
+
+  get 'pages/community_links'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
