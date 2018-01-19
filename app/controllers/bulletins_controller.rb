94b1bb59793de364c7504b9b08f4810422bbaced
Jordan Hudgens <jed.flynn.09@gmail.com>
2018-01-19 17:10:40 +0000

    Integrated new and create actions for portfolios

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -2,4 +2,26 @@ class BulletinsController < ApplicationController
   def index
     @bulletin_posts = Bulletin.all
   end
+  
+  def new
+    @bulletin_post = Bulletin.new
+  end
+  
+  def create
+    @bulletin_post = Bulletin.new(bulletin_params)
+    
+    respond_to do |format|
+      if @bulletin_post.save
+        format.html { redirect_to bulletins_path, notice: 'Your post was created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
+  private
+  
+    def bulletin_params
+      params.require(:bulletin).permit(:title, :body)
+    end
 end
