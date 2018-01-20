66899de3e1f78d29ddee702711a84f68af654053
Jordan Hudgens <jed.flynn.09@gmail.com>
2018-01-20 05:53:10 +0000

    Implemented image defaults for bulletin posts

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,6 +1,12 @@
 class Bulletin < ApplicationRecord
+  validates_presence_of :title, :body, :main_image, :thumb_image
   extend FriendlyId
   friendly_id :title, use: :slugged
   
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
