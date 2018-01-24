9e19d1e38f6d0d72e203e3eef370b34ce030a6bb
Jordan Hudgens <jed.flynn.09@gmail.com>
2018-01-24 06:13:21 +0000

    Completed styling for bulletin and rebuilt the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,46 @@
-20.times do |bulletin_post|
+3.times do |bulletin_post|
   Bulletin.create!(
-    title: "Bulletin post: #{bulletin_post}",
+    title: "X-Large bulletin post: #{bulletin_post}",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
 
-puts "20 bulletin posts created"
+3.times do |bulletin_post|
+  Bulletin.create!(
+    title: "Large bulletin post: #{bulletin_post}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+3.times do |bulletin_post|
+  Bulletin.create!(
+    title: "Medium bulletin post: #{bulletin_post}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+3.times do |bulletin_post|
+  Bulletin.create!(
+    title: "Small bulletin post: #{bulletin_post}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+puts "12 bulletin posts created"
+
+
+User.create!(email: "test@test.com",
+            name: "Jed Flynn",
+            password: "password",
+            roles: "site_admin"
+            )
+            
+puts "Site_admin user created"
