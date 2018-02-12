e23add1bec174399355f4ccef67664c57a91e21d
Jordan Hudgens <jed.flynn.09@gmail.com>
2018-02-12 03:38:56 +0000

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,18 +1,10 @@
 class Bulletin < ApplicationRecord
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
   
   mount_uploader :thumb_image, BulletinUploader
   mount_uploader :main_image, BulletinUploader
   
   def self.by_position
     order("position ASC")
-  end
-  
-  after_initialize :set_defaults
-  
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
